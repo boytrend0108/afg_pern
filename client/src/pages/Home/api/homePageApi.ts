237f6578b34262e72a1176f844a91ffff72a26bd
boytrend0108 <boytrend@gmail.com>
2024-05-14 09:48:52 +0300

    change test path

--- client/src/pages/Home/api/homePageApi.ts
+++ client/src/pages/Home/api/homePageApi.ts
@@ -1,5 +1,5 @@
 import { httpClient } from '../../../app/configs/httpConfig';
 
 export const leaveRequest = () => {
-  return httpClient.get('/1f8cb9d5-1641-482a-9882-569ce16eba38.webp');
+  return httpClient.get('/api/user');
 };
